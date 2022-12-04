3b721abcbf4810a6463cd0f5adb022ed89ed74aa
VeggyMeat <MacraeA@stpaulsschool.org.uk>
2022-12-04 15:31:44 +0000

    Time Estimation Added

--- Small Bundle Test.py
+++ Small Bundle Test.py
@@ -1,6 +1,8 @@
 import numpy as np
 import os
 import shutil
+import time
+
 # disables tensorflow debugging information
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
@@ -8,15 +10,22 @@ import tensorflow as tf
 
 counter = 0
 
-leading_zeros = 7
+data_in = "/home/amri123/Desktop/Training Data"
 data_to = "/home/amri123/Desktop/Training Data/Data"
 labels_to = "/home/amri123/Desktop/Training Data/Labels"
-total_files = 252702
 
-for root, dirs, files in os.walk("/home/amri123/Desktop/Training Data/train-clean-360/LibriSpeech/train-clean-360/192/128345"):
+start_time = time.time()
+leading_zeros = 7
+total_files = 104014
+one_percent = total_files // 100
+
+for root, dirs, files in os.walk("/home/amri123/Desktop/Training Data/train-clean-360/LibriSpeech/train-clean-360"):
+    # makes sure its the appropraite depth, where the files are
     if root.count("/") == 9:
         # makes sure they come in numerical order
         files.sort()
+
+        # loads the text files and the lines
         text_file = open(os.path.join(root, files[-1]), 'r')
         lines = text_file.readlines()
         text_file.close()
@@ -32,5 +41,12 @@ for root, dirs, files in os.walk("/home/amri123/Desktop/Training Data/train-clea
                 label_file = open(os.path.join(labels_to, (leading_zeros - len(str(counter))) * '0' + str(counter) + '.txt'), 'w')
                 label_file.write(' '.join(lines[n].split(' ')[1:]))
                 label_file.close()
+
+            if counter % one_percent == 0:
+                # prints an estimated time and percentage done message
+                percent_done = counter // one_percent
+                time_elapsed = time.time() - start_time
+                print(str(percent_done) + " percent done")
+                print("estimated " + str((time_elapsed / percent_done) * (100 - percent_done))[:7] + " seconds left")
 
 print(counter)
