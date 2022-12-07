40675f4a81cfc15d591dda8fc4a46a5adcac1e53
VeggyMeat <MacraeA@stpaulsschool.org.uk>
2022-12-07 15:23:20 +0000

    Data to Number List

---
+++ Label_Handling.py
@@ -0,0 +1,52 @@
+import os
+# disables tensorflow debugging information
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_io as tfio
+import numpy as np
+import soundfile as sf
+import matplotlib.pyplot as plt
+
+labels_dir = "/home/amri123/Desktop/Training Data/Labels/"
+
+
+def character_check():
+    # sets the starting variables and the number of files I have
+    total_files = 252702
+    chars = {}
+
+    # loops through a count up to file number
+    for count in range(total_files):
+        # opens the file of that one
+        label_file = open(os.path.join(labels_dir, (7 - len(str(count))) * '0' + str(count) + '.txt'), 'r')
+        data = label_file.read()
+
+        # goes through each character and adds that letter / increments that letter in the dictionary
+        for char in data:
+            if char in chars:
+                chars[char] += 1
+            else:
+                chars[char] = 1
+        
+        label_file.close()
+    
+    return chars
+
+
+# print(character_check())
+
+# characters I need to accept
+chars= "ABCDEFGHIJKLMNOPQRSTUVWXYZ' "
+
+# dictionary of character to number
+char_map = {chars[x]: x + 1 for x in range(len(chars))}
+
+
+def data_to_num(data):
+    # returns a list of the numbers associated to each character
+    return [char_map[char] for char in data]
+
+
+print(data_to_num("HELLO '"))
