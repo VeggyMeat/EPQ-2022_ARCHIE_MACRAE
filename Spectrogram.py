ef1dd649b7590313f7c1870435992f9dfe612000
VeggyMeat <MacraeA@stpaulsschool.org.uk>
2022-12-07 11:53:07 +0000

    Spectrogram Working

--- Spectrogram.py
+++ Spectrogram.py
@@ -1,20 +1,34 @@
+import os
+# disables tensorflow debugging information
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
 import tensorflow as tf
 from tensorflow import keras
 import tensorflow_io as tfio
+import numpy as np
+import soundfile as sf
 
 def to_spectogram(data_file_loc):
-    data_data = tf.io.read_file(data_file_loc)
+    # reads the data out 
+    data = tf.io.read_file(data_file_loc)
 
-    audio = tfio.audio.decode_flac(data_data)
+    # decodes the audio out of flac format
+    audio = tfio.audio.decode_flac(data, dtype=tf.int16)
+
+    # deletes redundant data in the encoding
     audio = tf.squeeze(audio, axis=-1)
 
+    # changes the data type to a float (able to handle non integers)
     audio = tf.cast(audio, tf.float32)
 
+    # actually creates the spectrogram
     spectrogram = tf.signal.stft(audio, frame_length=256, frame_step=160, fft_length=384)
 
+    # returns only the magnitude and gets rid of redundant scaling
     spectrogram = tf.abs(spectrogram)
-    # spectrogram = tf.math.pow(spectrogram, 0.5)
+    spectrogram = tf.math.pow(spectrogram, 0.5)
 
+    # normalises the data
     means = tf.math.reduce_mean(spectrogram, 1, keepdims=True)
     stddevs = tf.math.reduce_std(spectrogram, 1, keepdims=True)
     spectrogram = (spectrogram - means) / (stddevs + 1e-10)
