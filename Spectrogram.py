efd511e23b6c87b40c9b7fc5c6d6c688da01b860
VeggyMeat <MacraeA@stpaulsschool.org.uk>
2022-12-14 21:50:53 +0000

    Spectrogram file conversion running

--- Spectrogram.py
+++ Spectrogram.py
@@ -8,6 +8,8 @@ import tensorflow_io as tfio
 import numpy as np
 import soundfile as sf
 import matplotlib.pyplot as plt
+from Constants import *
+import time
 
 def to_spectrogram(data_file_loc):
     # reads the data out 
@@ -47,23 +49,51 @@ def display_spectrogram(spectrogram):
 
 
 def file_to_spectrogram(file_loc_in, file_loc_out):
-    # gets the spectrogram
+    # gets the spectrograms
     spectrogram = to_spectrogram(file_loc_in)
 
     # converts it to a numpy file
     numpy_spect = spectrogram.numpy()
 
     # saves it to the output file
-    np.savetxt(file_loc_out, numpy_spect)
+    np.savez_compressed(file_loc_out, numpy_spect)
 
 
 def read_spectrogram_file(file_loc):
     # reads it in as a numpy file
-    numpy_spect = np.loadtxt(file_loc, dtype=np.float32)
+    numpy_spect = np.load(file_loc)['arr_0']
 
     # converts it to a tensorflow tensor
     return tf.convert_to_tensor(numpy_spect, dtype=tf.float32)
 
-file_to_spectrogram("/home/amri123/Desktop/Training Data/Data/0000000.flac", "/home/amri123/Desktop/0000000.npy")
 
-print(read_spectrogram_file("/home/amri123/Desktop/0000000.npy"))
+def time_test_spectrogram():
+    start_time = time.time()
+
+    a = [to_spectrogram("/home/amri123/Desktop/Training Data/Data/000000" + str(x) + ".flac") for x in range(10)]
+
+    print(time.time() - start_time)
+
+    print(a)
+
+
+def spectrogram_files_conversion(dir_in, dir_out, n):
+    start_time = time.time()
+    # repeats n times
+    one_percent = n // 100
+    for counter in range(n):
+        # uses the file to spectrogram with the directories with laeding 0 files
+        file_to_spectrogram(os.path.join(dir_in, str(counter).zfill(7) + ".flac"), os.path.join(dir_out, str(counter).zfill(7) + ".npz"))
+
+        if counter % one_percent == 0:
+                # prints an estimated time and percentage done message
+                if counter != 0:
+                    percent_done = counter // one_percent
+                    print(str(percent_done) + " percent done")
+                    time_elapsed = time.time() - start_time
+                    print("estimated " + str((time_elapsed / percent_done) * (100 - percent_done) / 60)[:7] + " minutes left", end='\n\n')
+
+    print(str(time.time() - start_time) + " seconds taken")
+
+
+spectrogram_files_conversion("/home/amri123/Desktop/Training Data/Data", "/media/amri123/External SSD/Spectrograms", 252702)
